73ec6e22a711a92c428406ba564eb3dbd3885cda
Rger <mckeenweb@gmail.com>
2023-01-24 21:33:51 +0800

    loop events 2:41

--- 1-eventloop/1-ReadFile.js
+++ 1-eventloop/1-ReadFile.js
@@ -1,8 +1,10 @@
+// housed in folder [1-eventloop] is not going to work, only works as app.js in [NODECLASS] folder
+
 const { readFile } = require('fs')
 
 console.log('started a first task')
 // CHECK FILE PATH!!!!
-readFile('./content/result-sync.txt', 'utf8', (err, result) => {
+readFile('./content/first.txt', 'utf8', (err, result) => {
   if (err) {
     console.log(err)
     return
--- 1-eventloop/2-SetTimeOut.js
+++ 1-eventloop/2-SetTimeOut.js
@@ -1,3 +1,4 @@
+// housed in folder [1-eventloop] is not going to work, only works as app.js in [NODECLASS] folder
 // started operating system process
 console.log('first')
 setTimeout(() => {
