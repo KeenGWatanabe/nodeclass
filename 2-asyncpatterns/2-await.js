eedd61f2864f058d0a3acf2ac3e078bd07d54459
Rger <mckeenweb@gmail.com>
2023-01-25 21:20:22 +0800

    3:06 Async patterns

--- 2-asyncpatterns/2-await.js
+++ 2-asyncpatterns/2-await.js
@@ -1,13 +1,13 @@
-const {readFile, writeFile} = require ('fs').promises
-// const util = require('util')
-// const readFilePromise = util.promisify(readFile)
-// const writeFilePromise = util.promisify(writeFile)
+const {readFile, writeFile} = require ('fs')
+const util = require('util')
+const readFilePromise = util.promisify(readFile)
+const writeFilePromise = util.promisify(writeFile)
 
 const start = async() => {
   try {
-    const first = await readFile('./content/first.txt', 'utf8');
-    const second = await readFile('./content/test.txt', 'utf8');
-    await writeFile('./content/result-mind-grenade.txt', `This is awesome : ${first} ${second}`,{ flag: 'a'}
+    const first = await readFilePromise('./content/first.txt', 'utf8');
+    const second = await readFilePromise('./content/test.txt', 'utf8');
+    await writeFilePromise('./content/result-mind-grenade.txt', `This is awesome : ${first} ${second}`
     )
     console.log(first, second)
   } catch (error) {
--- 2-asyncpatterns/2-await2.js
+++ 2-asyncpatterns/2-await2.js
@@ -45,3 +45,4 @@ start()
 //     console.log(data)
 //   }
 // })
+
