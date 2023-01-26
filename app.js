2e46ec824791ce183c853b039fe6d2041d7cfdf4
Rger <mckeenweb@gmail.com>
2023-01-27 07:35:52 +0800

    3:35 16-stream.js

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
-const {createReadStream} = require('fs');
+const { createReadStream } = require('fs');
+const { result } = require('lodash');
 
-const stream = createReadStream('./content/big.txt')
+const stream = createReadStream('./content/big.txt', { 
+  highWaterMark: 90000, encoding: 'utf8'})
+
+// default 64kb
+// last buffer - remainder
+// highWaterMark - control size
+// const stream = creatReadStream('./content/big.txt', { highWaterMark: 90000})
+// const stream = creatReadStream('./content/big.txt', { encoding: 'utf8'})
+
 
 stream.on('data', (result) => {
   console.log(result)
 })
+stream.on('error',(err)=> console.log(err))
