6ac97f1309304afb1382affe8ae1f84a7ae92d90
Rger <mckeenweb@gmail.com>
2023-02-01 07:48:00 +0800

    18  create readStream in Chunks

--- 18-http-streamChunk.js
+++ 18-http-streamChunk.js
@@ -2,15 +2,14 @@ var http = require('http')
 var fs = require('fs')
 
 http
-  .createServer(function (req, res){
+  .createServer(function (req, res) {
     // const text = fs.readFileSync('./content/big.txt', 'utf8')
     // res.end(text)
-    const fiileStream = fs.createFileStream('./content/big.txt', 'utf8');
-    fiileStream.on('open',()=>{
+    const fiileStream = fs.createReadStream('./content/big.txt', 'utf8')
+    fiileStream.on('open', () => {
       fiileStream.pipe(res)
-
     })
-    fiileStream.on('error',(err)=>{
+    fiileStream.on('error', (err) => {
       res.end(err)
     })
 })
